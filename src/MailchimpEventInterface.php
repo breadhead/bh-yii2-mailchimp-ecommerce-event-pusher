6f704abc0525ceddac94745ffe4b19afc9adc74d
waytonoway <luckylifek@gmail.com>
2018-11-29 15:34:59 +0700

    Update MailchimpEventInterface.php

--- src/MailchimpEventInterface.php
+++ src/MailchimpEventInterface.php
@@ -3,5 +3,7 @@ namespace breadhead\mailchimp;
 
 interface MailchimpEventInterface
 {
+    public function supportEvent(string $eventType): bool;
+
     public function createAndSaveMailchimpEvent(string $eventType): MailchimpEvent;
 }
